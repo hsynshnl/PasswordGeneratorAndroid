a5b60b43066e8acea22ae6cbe6beac6438fbf075
HuseyinSahinli <huseyinsahinli2001@gmail.com>
2021-07-02 12:13:31 +0300

    Password Generator Begginer

--- app/src/main/java/com/tykhes/passwordgenerator/MainActivity.java
+++ app/src/main/java/com/tykhes/passwordgenerator/MainActivity.java
@@ -7,16 +7,10 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Switch;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Element;
-import org.w3c.dom.Text;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -24,57 +18,86 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     EditText passwordText;
-    ArrayList<String> LettersList=new ArrayList<>();
-    ArrayList<String> NumbersList=new ArrayList<String>();
+    ArrayList<String> lettersList=new ArrayList<>();
+    ArrayList<String> numbersList=new ArrayList<>();
+    ArrayList<String> symbolList=new ArrayList<>();
     Switch letterCheck;
-    Switch NumberCheck;
+    Switch numberCheck;
+    Switch symbolCheck;
+    Random random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        passwordText=findViewById(R.id.editTextTextPersonName);
-        letterCheck=findViewById(R.id.switch2);
-        NumberCheck=findViewById(R.id.switch1);
+        passwordText=findViewById(R.id.passwordText);
+        letterCheck=findViewById(R.id.lettersSwitch);
+        numberCheck=findViewById(R.id.numbersSwitch);
+        symbolCheck=findViewById(R.id.symbolSwitch);
         List<String> letterEz= Arrays.asList("Q","W","E","R","T","Y","U","I","O","P","G","U","A","D","S","F","G","H","J","K","L","Z","X","C","V","B","N","M","q","w","e","r","t","y","u","i","o","p","g","u","a","d","s","f","g","h","j","k","l","z","x","c","v","b","n","m");
         List<String> nummberEz=Arrays.asList("0","1","2","3","4","5","6","7","8","9");
-
-        LettersList.addAll(letterEz);
-        NumbersList.addAll(nummberEz);
+        List<String> symbolEz=Arrays.asList(".","-","+","=","_",",","!","@","$","#","*","%","<",">","[","]","{","}");
+        lettersList.addAll(letterEz);
+        numbersList.addAll(nummberEz);
+        symbolList.addAll(symbolEz);
         passwordText.setText("");
-
+        random=new Random();
     }
 
     public void Generate(View view){
         passwordText.getText().clear();
-        if (NumberCheck.isChecked()==true&&letterCheck.isChecked()==true){
-            for (int i=0;i<6;i++){
-                Random random=new Random();
-                int numberrandom=random.nextInt(NumbersList.size());
-                int numberrandoms=random.nextInt(LettersList.size());
-                passwordText.append(NumbersList.get(numberrandom)+LettersList.get(numberrandoms));
+
+        if (numberCheck.isChecked()==true&&letterCheck.isChecked()==true&&symbolCheck.isChecked()==true){
+            for (int i=0;i<4;i++){
+                int randomGet=random.nextInt(numbersList.size());
+                int randomGet2=random.nextInt(lettersList.size());
+                int randomGet3=random.nextInt(symbolList.size());
+                passwordText.append(numbersList.get(randomGet)+lettersList.get(randomGet2)+symbolList.get(randomGet3));
             }
         }
-        else if (NumberCheck.isChecked()==true){
-            for (int i=0;i<NumbersList.size();i++){
-                Random random=new Random();
-                int numberrandom=random.nextInt(NumbersList.size());
-                passwordText.append(NumbersList.get(numberrandom));
+        else if (numberCheck.isChecked()==true&&letterCheck.isChecked()==true){
+            for (int i=0;i<6;i++){
+                int randomGet=random.nextInt(numbersList.size());
+                int randomGet2=random.nextInt(lettersList.size());
+                passwordText.append(numbersList.get(randomGet)+lettersList.get(randomGet2));
+            }
+        }
+        else if (numberCheck.isChecked()==true&&symbolCheck.isChecked()==true) {
+            for (int i = 0; i < 6; i++) {
+                int randomGet = random.nextInt(numbersList.size());
+                int randomGet2 = random.nextInt(symbolList.size());
+                passwordText.append(numbersList.get(randomGet) + symbolList.get(randomGet2));
+            }
+        }
+        else if (letterCheck.isChecked()==true&&symbolCheck.isChecked()==true) {
+            for (int i=0;i<6;i++){
+                int randomGet = random.nextInt(lettersList.size());
+                int randomGet2 = random.nextInt(symbolList.size());
+                passwordText.append(lettersList.get(randomGet) + symbolList.get(randomGet2));
+            }
+        }
+        else if (numberCheck.isChecked()==true){
+            for (int i=0;i<numbersList.size();i++){
+                int randomGet=random.nextInt(numbersList.size());
+                passwordText.append(numbersList.get(randomGet));
             }
         }
         else if (letterCheck.isChecked()==true){
-            for (int i=0;i<NumbersList.size();i++){
-                Random random=new Random();
-                int numberrandom=random.nextInt(LettersList.size());
-                passwordText.append(LettersList.get(numberrandom));
+            for (int i=0;i<numbersList.size();i++){
+                int randomGet=random.nextInt(lettersList.size());
+                passwordText.append(lettersList.get(randomGet));
+            }
+        }
+        else if (symbolCheck.isChecked()==true){
+            for (int i=0;i<numbersList.size();i++){
+                int randomGet=random.nextInt(symbolList.size());
+                passwordText.append(symbolList.get(randomGet));
             }
         }
         else {
             passwordText.setText("Make Choose");
         }
-
     }
-
 
 public void Copy(View view){
     ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
